60427ef281625f9989060ba8585164e849e7ce0d
KDJProgger <kurt.de.jonghe2@gmail.com>
2017-06-23 14:30:37 +0200

    Adding timer delay

---
+++ time_delay.py
@@ -0,0 +1,25 @@
+import time
+
+i = 1
+
+print(10)
+time.sleep(i)
+print(9)
+time.sleep(i)
+print(8)
+time.sleep(i)
+print(7)
+time.sleep(i)
+print(6)
+time.sleep(i)
+print(5)
+time.sleep(i)
+print(4)
+time.sleep(i)
+print(3)
+time.sleep(i)
+print(2)
+time.sleep(i)
+print(1)
+time.sleep(i)
+print('BOEM BOEM BOEM')
